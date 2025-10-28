2e3c26e26c397be19becc4b55121024045b11e5d
twistednerd2019 <154948299+twistednerd2019@users.noreply.github.com>
2025-10-27 21:44:40 -0600

    Create disable_stub_dns_ubuntu.sh

---
+++ disable_stub_dns_ubuntu.sh
@@ -0,0 +1,12 @@
+# Stop the service
+sudo systemctl stop systemd-resolved
+
+# Disable it from starting on boot
+sudo systemctl disable systemd-resolved
+
+# Remove the symlink
+sudo rm -f /etc/resolv.conf
+
+# Create a new resolv.conf with your preferred DNS servers
+# echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
+# echo "nameserver 1.1.1.1" | sudo tee -a /etc/resolv.conf
